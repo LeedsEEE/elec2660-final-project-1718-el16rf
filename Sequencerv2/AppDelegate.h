405667eba994d5ea2f10b33995e84919faf9ea09
Reuben Frankel [el16rf] <el16rf@campus-10-41-70-20.wireless.leeds.ac.uk>
2017-11-21 17:35:48 +0000

    Initial Commit

---
+++ Sequencerv2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sequencerv2
+//
+//  Created by Reuben Frankel [el16rf] on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sequencerv2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sequencerv2
+//
+//  Created by Reuben Frankel [el16rf] on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
